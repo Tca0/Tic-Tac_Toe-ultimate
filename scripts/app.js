4bd1e63cf81a9f642c2943cb01b59883bb8c2d38
Tca0 <taher.khtieb@gmail.com>
2023-12-19 22:11:46 +0000

    removing comments changing getChildren function name

--- scripts/app.js
+++ scripts/app.js
@@ -15,13 +15,13 @@ function init() {
   ];
   let nextMoveOnBoard = null;
 
-  function getChildren(board) {
+  function getChildrenNodes(board) {
     return Object.values(board.children);
   }
 
   function addClickEventListener(board) {
     board.classList.add("available");
-    const cells = getChildren(board);
+    const cells = getChildrenNodes(board);
     cells.forEach((cell) => {
       if (cell.classList.length === 1)
         cell.addEventListener("click", handleCellClick, { once: true });
@@ -30,7 +30,7 @@ function init() {
 
   function removeClickEventListener(board) {
     board.classList.remove("available");
-    const cells = getChildren(board);
+    const cells = getChildrenNodes(board);
     cells.forEach((cell) => {
       cell.removeEventListener("click", handleCellClick);
     });
@@ -43,13 +43,13 @@ function init() {
     nextMoveOnBoard = parseInt(cell.getAttribute("data-index"));
     cell.textContent = currentPlayer;
     cell.classList.add(currentPlayer);
-    // ? afetr local win check for globla win or draw
-    // ! after local draw check only for global draw
-    // * for win or drwa same conditions will apply for global win/draw
-    //  & change the name of function to check for win or drwa then apply same function for global win/draw
-    // ^ get children function return only direct children node so we can use it now to either locals or cells
-    const localWin = checkWin(board)
-    const localDraw = checkDraw(board)
+
+    // ! Check for local win or drwa
+    // Prepear for the next move
+    // ? Check for total win or drwa
+
+    const localWin = checkWin(board);
+    const localDraw = checkDraw(board);
     if (localWin) {
       handleLocalWinOrDraw(
         board,
@@ -60,28 +60,24 @@ function init() {
       handleLocalWinOrDraw(board, `drwa`, `drwa`);
     }
     checkForNextGrid(nextMoveOnBoard);
-    // check for total win or toal draw
+
     if (localWin || localDraw) {
       const globalWin = checkWin(globalBoard);
       const globalDrwa = checkDraw(globalBoard);
-      gameOver = (globalWin || globalDrwa) ? true : false;
+      gameOver = globalWin || globalDrwa ? true : false;
       if (globalWin) {
         console.log(`game over, the player ${currentPlayer} wins`);
       }
       if (globalDrwa) {
-        // console.log(Object.values(globalBoard.children))
-        // console.log(globalBoard.classList)
         console.log("Game over, no winner");
       }
     }
 
     currentPlayer = currentPlayer === "X" ? "O" : "X";
-    
   }
 
   function checkWin(board) {
-    // const cells = localBoard.querySelectorAll(".cell");
-    const cells = Object.values(board.children);
+    const cells = getChildrenNodes(board);
     return winningConditions.some((condition) => {
       return condition.every((index) =>
         cells[index].classList.contains(currentPlayer)
@@ -90,21 +86,20 @@ function init() {
   }
 
   function checkDraw(board) {
-    // const cells = Array.from(board.querySelectorAll(".cell"));
-    const cells = Object.values(board.children);
+    const cells = getChildrenNodes(board);
     if (board.classList.contains("global")) {
-      console.log(`checking on global board`)
-      console.log(cells)
+      console.log(`checking on global board`);
+      console.log(cells);
       return !cells.some((cell) => {
-        console.log(cell)
-        console.log(cell.classList.contains('available'))
-        return cell.classList.contains('available')
+        console.log(cell);
+        console.log(cell.classList.contains("available"));
+        return cell.classList.contains("available");
       });
     } else {
       return cells.every((cell) => cell.classList.length === 2);
     }
-    // return cells.every((cell) => cell.classList.length === 2);
   }
+
   function handleLocalWinOrDraw(board, messageToUpdate, classToUpdate) {
     board.classList.add(classToUpdate);
     message = messageToUpdate;
